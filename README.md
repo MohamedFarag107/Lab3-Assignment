4788bea2895f9d1b2ae44fbe87305c39d071e1eb
Mohamed Farag <mf1456@fayoum.edu.eg>
2023-03-21 15:51:55 +0200

    Lab3 Assignment

---
+++ README.md
@@ -0,0 +1,9 @@
+# flutter_app
+
+A new Flutter project.
+
+## Getting Started
+
+
+author: Mohamed Farag
+Lab3 Assignment 
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('First Application'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: const Center(
+        child: Text("Mohamed Farag Mohamed"),
+      ),
+    ),
+  ));
+}
